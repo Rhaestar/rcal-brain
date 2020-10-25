c084439a10e7197509508b05535af717ba310b23
Rhaestar <simon.regourd@epita.fr>
2020-10-25 17:18:05 +0100

    vtk: add tumor vis

--- vision.py
+++ vision.py
@@ -4,8 +4,9 @@ reader = vtk.vtkMetaImageReader()
 reader.SetFileName('Data/brats.mha')
 reader.Update()
 
-polyMapper = vtk.vtkSmartVolumeMapper()
-polyMapper.SetInputConnection(reader.GetOutputPort())
+reader2 = vtk.vtkMetaImageReader()
+reader2.SetFileName('Data/seg.mha')
+reader2.Update()
 
 opacity_transfer_function = vtk.vtkPiecewiseFunction()
 opacity_transfer_function.AddPoint(0.0, 0.0)
@@ -13,19 +14,12 @@ opacity_transfer_function.AddPoint(90, 0.0)
 opacity_transfer_function.AddPoint(100, 0.2)
 opacity_transfer_function.AddPoint(120, 0.0)
 
-volume_property = vtk.vtkVolumeProperty()
-volume_property.SetScalarOpacity(opacity_transfer_function)
-
-actorVolume = vtk.vtkVolume()
-actorVolume.SetMapper(polyMapper)
-actorVolume.SetProperty(volume_property)
-
+#Render brain as wireframe
 marchingCubes = vtk.vtkMarchingCubes()
 marchingCubes.SetInputConnection(reader.GetOutputPort())
 marchingCubes.SetValue(0, 135)
 marchingCubes.ComputeNormalsOn()
 marchingCubes.Update()
-
 
 polyMapper = vtk.vtkPolyDataMapper()
 polyMapper.SetInputConnection(marchingCubes.GetOutputPort())
@@ -36,11 +30,29 @@ actor = vtk.vtkActor()
 actor.SetMapper(polyMapper)
 actor.GetProperty().SetRepresentationToWireframe()
 
+#Render tumor
+marchingCubes2 = vtk.vtkMarchingCubes()
+marchingCubes2.SetInputConnection(reader2.GetOutputPort())
+marchingCubes2.SetValue(0, 135)
+marchingCubes2.ComputeNormalsOn()
+marchingCubes2.Update()
+
+polyMapper2 = vtk.vtkPolyDataMapper()
+polyMapper2.SetInputConnection(marchingCubes2.GetOutputPort())
+polyMapper2.SetScalarVisibility(0)
+polyMapper2.Update()
+
+colors = vtk.vtkNamedColors()
+actor2 = vtk.vtkActor()
+actor2.SetMapper(polyMapper2)
+actor2.GetProperty().SetDiffuseColor(colors.GetColor3d("Tomato"))
+#actor2.GetProperty().SetRepresentationToWireframe()
+
 renderer = vtk.vtkRenderer()
 renderer.SetBackground(0.5, 0.5, 0.5)
 renderer.ResetCamera()
 renderer.AddActor(actor)
-renderer.AddActor(actorVolume)
+renderer.AddActor(actor2)
 
 window = vtk.vtkRenderWindow()
 window.AddRenderer(renderer)
